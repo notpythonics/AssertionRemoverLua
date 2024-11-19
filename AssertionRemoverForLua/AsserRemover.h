272d8ed94ef267e46ac92a52e5613be0868368ea
moamen_alali <myiopasdfkvkk@gmail.com>
2024-11-19 20:06:58 +0300

    Refactoring

--- AssertionRemoverForLua/AsserRemover.h
+++ AssertionRemoverForLua/AsserRemover.h
@@ -5,11 +5,14 @@
 #include <fstream>
 #include <filesystem>
 #include <vector>
+#include <string_view>
 
-inline int removeAsserFromFile(std::filesystem::path pathToFile);
-inline int removeAsserFromDir(std::filesystem::path pathToDir);
+inline constexpr std::string_view BLACK_LISTED_WORD{ "assert" };
 
-inline int startAsserRemover(std::filesystem::path pathToFile) {
+inline int removeAsserFromFile(const std::filesystem::path& pathToFile);
+inline int removeAsserFromDir(const std::filesystem::path& pathToDir);
+
+inline int startAsserRemover(const std::filesystem::path& pathToFile) {
 	// dir
 	if (std::filesystem::is_directory(pathToFile)) {
 		return removeAsserFromDir(pathToFile);
@@ -19,7 +22,7 @@ inline int startAsserRemover(std::filesystem::path pathToFile) {
 	}
 }
 
-inline int removeAsserFromFile(std::filesystem::path pathToFile) {
+inline int removeAsserFromFile(const std::filesystem::path& pathToFile) {
 	const auto* c_pathToFile{ pathToFile.c_str() };
 	// lines that have no assertions
 	std::vector<std::string> linesWithoutAsser{};
@@ -30,7 +33,7 @@ inline int removeAsserFromFile(std::filesystem::path pathToFile) {
 	std::ifstream ifile{ c_pathToFile };
 	// push non-asserted lines to vector
 	for (std::string line; std::getline(ifile, line);) {
-		if (line.find("assert") != line.npos)
+		if (line.find(BLACK_LISTED_WORD) != line.npos)
 			++numAssers;
 		else
 			linesWithoutAsser.push_back(line);
@@ -46,13 +49,17 @@ inline int removeAsserFromFile(std::filesystem::path pathToFile) {
 	return numAssers;
 }
 
-inline int removeAsserFromDir(std::filesystem::path pathToDir) {
+inline int removeAsserFromDir(const std::filesystem::path& pathToDir) {
 	// how many assertions were removed
 	int numAssers{};
 
 	// traverse the dir
 	std::filesystem::directory_iterator luaRootDir_iter{ pathToDir };
 	for (const std::filesystem::directory_entry& entry : luaRootDir_iter) {
+		const auto& path{ entry.path() };
+		if (std::filesystem::is_directory(path)) {
+			numAssers += removeAsserFromDir(path);
+		}
 		numAssers += removeAsserFromFile(entry.path());
 	}
 
