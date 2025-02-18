f06c66bda636a5d71fe5860de756e65a2e44a88c
moamen_alali <myiopasdfkvkk@gmail.com>
2025-02-18 04:58:11 +0300

    Refactoring

--- AssertionRemoverForLua/AsserRemover.h
+++ AssertionRemoverForLua/AsserRemover.h
@@ -7,27 +7,27 @@
 #include <vector>
 #include <string_view>
 
+namespace fs = std::filesystem;
+
+
 inline constexpr std::string_view BLACK_LISTED_WORD{ "assert" };
 
-inline int removeAsserFromFile(const std::filesystem::path& pathToFile);
-inline int removeAsserFromDir(const std::filesystem::path& pathToDir);
+inline int removeAsserFromFile(const fs::path& pathToFile);
+inline int removeAsserFromDir(const fs::path& pathToDir);
 
-inline int startAsserRemover(const std::filesystem::path& pathToFile) {
-	// dir
-	if (std::filesystem::is_directory(pathToFile)) {
-		return removeAsserFromDir(pathToFile);
-	} // file
-	else {
-		return removeAsserFromFile(pathToFile);
-	}
+inline int startAsserRemover(const fs::path& path) {
+	if (fs::is_directory(path))
+		return removeAsserFromDir(path);
+	else
+		return removeAsserFromFile(path);
 }
 
-inline int removeAsserFromFile(const std::filesystem::path& pathToFile) {
+inline int removeAsserFromFile(const fs::path& pathToFile) {
 	const auto* c_pathToFile{ pathToFile.c_str() };
-	// lines that have no assertions
+	// non-asserted lines
 	std::vector<std::string> linesWithoutAsser{};
 	linesWithoutAsser.reserve(20);
-	// how many assertions were removed
+	// assertions removed
 	int numAssers{};
 
 	std::ifstream ifile{ c_pathToFile };
@@ -51,15 +51,15 @@ inline int removeAsserFromFile(const std::filesystem::path& pathToFile) {
 	return numAssers;
 }
 
-inline int removeAsserFromDir(const std::filesystem::path& pathToDir) {
+inline int removeAsserFromDir(const fs::path& pathToDir) {
 	// how many assertions were removed
 	int numAssers{};
 
 	// traverse the dir
-	std::filesystem::directory_iterator luaRootDir_iter{ pathToDir };
-	for (const std::filesystem::directory_entry& entry : luaRootDir_iter) {
+	fs::directory_iterator luaRootDir_iter{ pathToDir };
+	for (const fs::directory_entry& entry : luaRootDir_iter) {
 		const auto& path{ entry.path() };
-		if (std::filesystem::is_directory(path)) {
+		if (fs::is_directory(path)) {
 			numAssers += removeAsserFromDir(path); // recursion
 		}
 		else {
--- AssertionRemoverForLua/main.cpp
+++ AssertionRemoverForLua/main.cpp
@@ -3,22 +3,17 @@
 #include <iostream>
 
 int main(int argc, char* argv[]) {
-	// first argument
 	if (argc != 2) {
 		std::cout << "Missing a path.\n";
 		return -1;
-	} // just one arg
-	else if (argc > 2) {
-		std::cout << "Too many args.\n";
-		return -1;
 	}
 
-	// path to an existing file/dir
-	std::filesystem::path pathToFile{ argv[1] };
+	fs::path pathToFile{ argv[1] };
 	pathToFile.make_preferred();
 	std::cout << "Preferred Path: " << pathToFile << '\n';
-	if (!std::filesystem::exists(pathToFile)) {
-		std::cout << "Invalid Path.\n";
+
+	if (!fs::exists(pathToFile)) {
+		std::cout << "Invalid path.\n";
 		return -1;
 	}
 
